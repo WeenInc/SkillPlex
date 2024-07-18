33a9c6d50c38edac9bd32408a5a6f68ea05540dc
leonardo.cuco <leonardo.cuco@gmail.com>
2024-07-18 16:15:07 +0200

    Make SkillPlex parameters optional

--- SkillPlex/__init__.py
+++ SkillPlex/__init__.py
@@ -1,5 +1,5 @@
 from .openai_client import OpenAIClient
 
 class SkillPlex:
-    def __init__(self, db_url, openai_api_key, skillplex_endpoint):
+    def __init__(self, db_url=None, openai_api_key=None, skillplex_endpoint=None):
         self.openai = OpenAIClient(db_url, openai_api_key, skillplex_endpoint)
--- SkillPlex/openai_client/__init__.py
+++ SkillPlex/openai_client/__init__.py
@@ -1,7 +1,15 @@
 from .openai_wrapper import OpenAIWrapper
 from .chat import Chat
+import os
 
 class OpenAIClient:
-    def __init__(self, db_url, openai_api_key, skillplex_endpoint):
+    def __init__(self, db_url=None, openai_api_key=None, skillplex_endpoint=None):
+        if db_url is None:
+            db_url = os.environ.get('DB_URL')  # Set a sensible default or handle it as needed
+        if openai_api_key is None:
+            openai_api_key = os.environ.get('OPENAI_API_KEY')
+        if skillplex_endpoint is None:
+            skillplex_endpoint = os.environ.get('SKILLPLEX_ENDPOINT')
+
         self.chat = Chat(db_url, openai_api_key, skillplex_endpoint)
         self.wrapper = OpenAIWrapper(db_url, openai_api_key, skillplex_endpoint)
